9923fa26d0d60ee6946f7cae53bfc71c37c4ebde
Moh husaini <52305289+MohHusain@users.noreply.github.com>
2020-05-11 16:07:33 +0700

    Add files via upload

---
+++ htdos/praktik2_tipe_data.php
@@ -0,0 +1,22 @@
+<?php 
+$string = "Nama saya Moh Husaini";
+?>
+
+<?php
+$int_pertama = 05;
+$int_kedua = 07;
+$int_ketiga = 1999;
+?>
+
+<?php
+$int_desimal = 48.5;
+?>
+
+<?php
+$boolean_true = true;
+$boolean_false = false;
+?>
+
+<?php
+$array_tipe = array("komputer","jaringan","desain","fotografi")
+?>
